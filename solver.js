2cb99bba7c1db27404effde77c2924a77cb366f3
Jahred Hope <jahred.hope@xero.com>
2016-05-10 18:06:15 +1000

    Store all values in a single array and use location 'pointers'

--- solver.js
+++ solver.js
@@ -1,64 +1,146 @@
+function base3rd(r) {return Math.floor(r/3)*3;}
+function r3(i) {return i%3;}
+function c3(i) {return Math.floor(i/3);}
+function r9(i) {return i%9;}
+function c9(i) {return Math.floor(i/9);}
+
 module.exports = function solve(str) {
   var done = false;
-  const locationKeys = [0,1,2,3,4,5,6,7,8];
+  var solved = false;
+  var foundFromOnly = 0;
+  var foundFromRows = 0;
+  var foundFromCols = 0;
+  var foundFromGroup = 0;
+
   const all = '123456789';
+  const locationKeys = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const keys = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const groSel  = [0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8];
+  const colSel  = [0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8];
+  const rowSel   = [0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8];
+  const groupStarts = [0, 3, 6, 27, 30, 33, 54, 57, 60];
+  const cellStarts = [0, 1, 2, 9, 10, 11, 18, 19, 20];
+
+  const full = (new Array(81)).fill(all,0,81);
+
+  const rows = locationKeys.map(rowNo => locationKeys.map(cellNo => (rowNo * 9) + cellNo));
+  const cols = locationKeys.map(colNo => locationKeys.map(cellNo => (cellNo * 9) + colNo))
+  const groups = locationKeys.map(groupNo => locationKeys.map(cellNo => groupStarts[groupNo] + cellStarts[cellNo]))
+
+
   const options = [[], [], [], [], [], [], [], [], []].map(() => [all,all,all,all,all,all,all,all,all]);
 
-  function base3rd(r) {return Math.floor(r/3)*3;}
-  function r3(i) {return i%3;}
-  function c3(i) {return Math.floor(i/3);}
-  function r9(i) {return i%9;}
-  function c9(i) {return Math.floor(i/9);}
 
-  function place(r,c,v) {
-    // console.log(`Placing ${v} at ${r}|${c}`);
-    options[r][c] = v;
-    locationKeys.forEach(i=>remove(i,c,v));
-    locationKeys.forEach(i=>remove(r,i,v));
-    locationKeys.forEach(i=> {
-      remove(base3rd(r)+r3(i), base3rd(c)+c3(i), v)
-    });
-  }
-
-  function remove(r,c,v) {
-    // console.log('removing', r,c, options[r][c]);
-    if( typeof options[r][c] === 'string') {
-      options[r][c] = options[r][c].replace(v,'');
+  function place(l,v) {
+    // console.log(`Placing ${v} at ${l}`);
+    function rem(l) {
+      if( typeof full[l] === 'string') {
+        full[l] = full[l].replace(v,'');
+      }
     }
+    full[l] = v;
+    rows[rowSel[l]].forEach(rem);
+    cols[colSel[l]].forEach(rem);
+    groups[groSel[l]].forEach(rem);
   }
 
   function printGrid() {
-    var printedGrid = 'GRID\n';
-    options.forEach((row)=> {
-      row.forEach((cell) => {
-        printedGrid += (typeof cell === 'number' ? cell : ' ');
-      })
-      printedGrid += '\n';
+    console.log('PRINT GRID');
+    var result = '';
+    full.forEach((cell, index) => {
+      // console.log(index%9);
+      if(typeof cell === 'number') {
+        result += cell.toString();
+      } else {
+        result += '-';
+      }
+      result += (((index+1) % 9) ? ' ' : '\n');
     });
-    console.log(printedGrid);
+    console.log(result);
   }
 
   function checkThatShahizle() {
     var solvedCount = 0;
     var solutionsFoundThisRun = 0;
-    locationKeys.forEach(r => {
-      locationKeys.forEach(c => {
-        var v = options[r][c];
-        if(typeof v !== 'number') {
-          if(v.length === 1) {
-            solutionsFoundThisRun++;
-            place(r,c,parseInt(v));
-          }
-        } else {
-          solvedCount++;
+    full.forEach((cell, index) => {
+      var v = full[index];
+      if(typeof v !== 'number') {
+        if(v.length === 1) {
+          solutionsFoundThisRun++;
+          foundFromOnly++;
+          place(index,parseInt(v));
         }
+      } else {
+        solvedCount++;
+      }
+    });
+
+
+    groups.forEach(group => {
+      keys.forEach(key => {
+        var gLoc = -1;
+        var found = group.some(l => {
+          if(typeof full[l] === 'string' && full[l].includes(key)) {
+            if(gLoc == -1) {
+              gLoc = l;
+            } else {
+              return true;
+            }
+          }
+          return false;
+        });
+      if(!found && gLoc !== -1) {
+        solutionsFoundThisRun++;
+        foundFromGroup++;
+        place(gLoc, key);
+      }
       });
     });
+    rows.forEach(group => {
+      keys.forEach(key => {
+        var gLoc = -1;
+        var found = group.some(l => {
+          if(typeof full[l] === 'string' && full[l].includes(key)) {
+            if(gLoc == -1) {
+              gLoc = l;
+            } else {
+              return true;
+            }
+          }
+          return false;
+        });
+      if(!found && gLoc !== -1) {
+        solutionsFoundThisRun++;
+        foundFromRows++;
+        place(gLoc, key);
+      }
+      });
+    });
+    cols.forEach(column => {
+      keys.forEach(key => {
+        var gLoc = -1;
+        var found = column.some(l => {
+          if(typeof full[l] === 'string' && full[l].includes(key)) {
+            if(gLoc == -1) {
+              gLoc = l;
+            } else {
+              return true;
+            }
+          }
+          return false;
+        });
+      if(!found && gLoc !== -1) {
+        foundFromCols++;
+        solutionsFoundThisRun++;
+        place(gLoc, key);
+      }
+      });
+    });
+
     if(solvedCount === 81) {
-      console.log("WE DONE!!!!");
+      solved = true;
       done = true;
     } else if(solutionsFoundThisRun === 0) {
-      console.log("I can't solve this :(");
       done = true;
     }
     // printGrid();
@@ -66,12 +148,16 @@ module.exports = function solve(str) {
 
   str.split('').map((c,i) => {
     if(/\d/.test(c)) {
-      place(c9(i),r9(i),parseInt(c));
+      place(i,parseInt(c));
     }
   });
+  printGrid();
+
   while(!done) {
     checkThatShahizle();
   }
+
   printGrid();
+  console.log(`${solved ? 'Solved' : 'Unsolved'} from: Only:${foundFromOnly} Columns:${foundFromCols} Rows:${foundFromRows} Groups:${foundFromGroup}`);
   return(options);
 };
